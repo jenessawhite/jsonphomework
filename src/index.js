bb7652db8f5979b6f74909c99b11612540253db3
Jenessa <jenessaw31@gmail.com>
2016-11-17 22:46:07 -0600

    stylings completed. site running

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function loadIt(){
-
 var pageReq = $('#pageReq').val();
-
+if (pageReq == '') {
+  alert('Your search terms were not valid')
+} else {
 $.getJSON(
   "https://www.reddit.com/r/" + pageReq + ".json",
   function goGetIt(data)
@@ -10,24 +11,29 @@ $.getJSON(
     $.each(
       data.data.children.slice(0, 10),
       function (request, result) {
-        // var title = $('<h2></h2>')
-        group.append($('<h2></h2>').text(result.data.title));
+        // var thumbnail = $('<img/>')
+        if (result.data.thumbnail == 'self' || result.data.thumbnail == null){
+          group.append($('<img class="thumbnail"/>').attr('src','https://cdn1.iconfinder.com/data/icons/school-supplies-3/64/folder_denied_stop_not_found_deleted_error-128.png'));
+        } else {
+          group.append($('<img class="thumbnail"/>').attr('src',result.data.thumbnail));
+        }
+        group.append($('<br>') );
         // var url = $('<a href='#'></a>')
-        group.append($('<a><a>').attr('href',result.data.url).text(result.data.url));
-        // var url = $('<img/>')
-        group.append($('<img/>').attr('src',result.data.thumbnail));
+        group.append($('<a class="title"></a>').attr('href',result.data.url).attr('target',"_blank").text(result.data.title));
+        // var description = $('<p></p>')
+        group.append($('<p class="description"></p>').text("Click to read more!"));
         // // var ups = $('<p></p>')
-        group.append($('<p></p>').text(result.data.ups) );
-        // // var downs = $('<p></p>')
-        group.append($('<p></p>').text(result.data.downs) );
-      }
-    )
-    $("#content").empty().append( group );
-  }
-)
+        group.append($('<div class="ups"></div>').text(result.data.ups) );
+        // // var num_comments = $('<p></p>')
+        group.append($('<div class="comments"></div>').text(result.data.num_comments) );
+        group.append($('<hr>') );
+      })
+    $("#content").empty().append(group);
+  })
 // .success(function() { alert("second success"); })
-.error(function() { alert("error"); })
+.error(function() {alert("Your search terms were not valid"); })
 // .complete(function() { alert("complete"); });
+}
 }
 
 $('#loadReq').on("click", loadIt);
